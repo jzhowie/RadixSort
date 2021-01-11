e3edddd672ba3a2defe48b6971ca3f7534dfda92
jzhowie <jzhou20@stuy.edu>
2021-01-11 14:16:19 -0500

    Initialized length()

---
+++ Radix.java
@@ -0,0 +1,17 @@
+//TODO: Nth, merge, sort
+//ISSUES:
+
+public class Radix extends MyLinkedList {
+
+public static int nth(int n, int col) {
+	return 0; //placeholder
+}
+
+public static int length(int n) {
+	return (int) Math.log10(n) + 1;
+}
+
+public static void merge(MyLinkedList original, MyLinkedList[] buckets) {
+
+}
+}
