d1c144f86dc0e848b2a53e990eaf6919f3bc10e4
jzhowie <jzhou20@stuy.edu>
2021-01-12 12:56:48 -0500

    Changed param names; had length return a positive int for negative nums

--- Radix.java
+++ Radix.java
@@ -8,7 +8,7 @@ public static int nth(int n, int col) {
 }
 
 public static int length(int n) {
-	return (int) Math.log10(n) + 1;
+	return (int) Math.log10(Math.abs(n)) + 1;
 }
 
 public static void merge(SortableLinkedList original, SortableLinkedList[] buckets) {
@@ -17,22 +17,48 @@ public static void merge(SortableLinkedList original, SortableLinkedList[] bucke
 	}
 }
 
-public static void radixSortSimple(SortableLinkedList original) {
+public static void radixSortSimple(SortableLinkedList data) {
 	SortableLinkedList bucket[] = new SortableLinkedList[10];
 	for (int i = 0; i < 10; i++) {
 		bucket[i] = new SortableLinkedList();
 	}
 	int largestLength = 1;
-	int size = original.size();
+	int size = data.size();
 	for (int col = 0; col < largestLength; col++) {
 		for (int i = 0; i < size; i++) {
-			if (col == 0 && length(original.get(0)) > largestLength) {
-				largestLength = length(original.get(0));
+			if (col == 0 && length(data.get(0)) > largestLength) {
+				largestLength = length(data.get(0));
 			}
-			bucket[nth(original.get(0), col)].add(original.get(0));
-			original.remove(0);
+			bucket[nth(data.get(0), col)].add(data.get(0));
+			data.remove(0);
 		}
-		merge(original, bucket);
+		merge(data, bucket);
+	}
+}
+
+public static void radixSort(SortableLinkedList data) {
+	SortableLinkedList bucket[] = new SortableLinkedList[10];
+	for (int i = 0; i < 10; i++) {
+		bucket[i] = new SortableLinkedList();
+	}
+	int largestLength = 1;
+	int size = data.size();
+
+	for (int col = 0; col < largestLength; col++) {
+		for (int i = 0; i < size; i++) {
+			if (col == 0 && length(data.get(0)) > largestLength) {
+				largestLength = length(data.get(0));
+			}
+			if (data.get(0) > 0) {
+				bucket[nth(data.get(0), col)].add(data.get(0));
+				data.remove(0);
+			}
+			else {
+				bucket[0].add(data.get(0));
+				data.remove(0);
+			}
+		}
+		merge(data, bucket);
 	}
 }
 }
