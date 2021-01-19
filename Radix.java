2f48166962dbe7055ea886abe0f3f4ad5f858926
jzhowie <jzhou20@stuy.edu>
2021-01-18 19:35:13 -0500

    Fixed null errors

--- Radix.java
+++ Radix.java
@@ -12,7 +12,7 @@ public static int length(int n) {
 }
 
 public static void merge(SortableLinkedList original, SortableLinkedList[] buckets) {
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < buckets.length; i++) {
 		original.extend(buckets[i]);
 	}
 }
@@ -42,13 +42,11 @@ public static void radixSort(SortableLinkedList data) {
 	}
 	int largestLength = 1;
 	int size = data.size();
-
 	for (int col = 0; col < largestLength; col++) {
 		for (int i = 0; i < size; i++) {
 			if (col == 0 && length(data.get(0)) > largestLength) {
 				largestLength = length(data.get(0));
 			}
-
 			if (data.get(0) < 0) {
 				bucket[9 - nth(data.get(0), col)].add(data.remove(0));
 			}
@@ -56,11 +54,8 @@ public static void radixSort(SortableLinkedList data) {
 				bucket[10].add(data.remove(0));
 			}
 		}
-		if (col == 0) {
-			radixSortSimple(bucket[10]);
-		}
+		radixSortSimple(bucket[10]);
 		merge(data, bucket);
 	}
-	data.extend(bucket[10]);
 }
 }
